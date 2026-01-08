faa37bc6ac1ba2a2177f48d02dd139881f15f597
trannhattan5 <nhattan11205@gmail.com>
2026-01-08 18:07:20 +0700

    Viết API đăng nhập (Signin)

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,43 +1,132 @@
 import bcrypt from 'bcrypt'
 import User from '../models/User.js';
-  
-export const signUp = async (req, res)=>{
-    try{
-        const {username, password, email , firstName, lastName}= req.body;
+import jwt from 'jsonwebtoken';
+import crypto from 'crypto'
+import Session from '../models/Session.js';
 
-        if (!username || !password ||!email || !firstName || !lastName) {
+const ACCESS_TOKEN_TTL = '30m'; //thường < 15m
+const REFRESH_TOKEN_TTL = 14 * 24 * 60 * 60 * 1000; //14 ngày
+
+export const signUp = async (req, res) => {
+    try {
+        const {
+            username,
+            password,
+            email,
+            firstName,
+            lastName
+        } = req.body;
+
+        if (!username || !password || !email || !firstName || !lastName) {
             return res.status(400).json({
-                message:"Không thể thiếu username, password, firstName và lastName"
+                message: "Không thể thiếu username, password, firstName và lastName"
             })
         }
         //kiểm tra user có tồn tại chưa
-        const duplicate = await User.findOne({username})
+        const duplicate = await User.findOne({
+            username
+        })
 
         if (duplicate) {
             return res.status(409).json({
-                message:"Username đã tồn tại"
+                message: "Username đã tồn tại"
             })
         }
         // mã hóa password
-        const hashedPassword = await bcrypt.hash(password,10); //salt = 10 
+        const hashedPassword = await bcrypt.hash(password, 10); //salt = 10 
         // console.log("hashpassword"+hashedPassword);
-        
+
         //tạo user mới
 
         await User.create({
             username,
             hashedPassword,
-            email, 
-            displayName:`${firstName}${lastName}`
+            email,
+            displayName: `${firstName}${lastName}`
         })
 
         //return
         return res.sendStatus(204);
-    }catch(error){
+    } catch (error) {
         console.error('Lỗi khi gọi signUp', error);
         return res.sendStatus(500).json({
-            message:"Lỗi hệ thống"
+            message: "Lỗi hệ thống"
         })
+
+    }
+}
+
+export const signIn = async (req, res) => {
+
+    try {
+        // Lấy inputs
+        const {
+            username,
+            password
+        } = req.body;
+        if (!username || !password) {
+            return res.status(400).json({
+                message: "Thiếu username hoặc password"
+            })
+        }
         
+
+        //lấy hashedpassword trong db để so sánh password user nhập
+        const user = await User.findOne({
+            username
+        });
+
+        if (!user) {
+            return res.status(401).json({
+                message: "username hoặc password không chính xác"
+            })
+        }
+        //kiểm trả password
+        const passwordCorrect = await bcrypt.compare(password, user.hashedPassword)
+                console.log("Password"+ password);
+                console.log("Password"+ user.hashedPassword);
+
+        if (!passwordCorrect) {
+            return res.status(401).json({
+                message: "Username hoặc Password không chính xác"
+            })
+        }
+
+        //nếu khớp =>  tạo accesstoken với JWT
+        const accessToken = jwt.sign({
+            userId: user._id
+        }, process.env.ACCESS_TOKEN_SECRET, {
+            expiresIn: ACCESS_TOKEN_TTL
+        })
+
+
+        //tạo refreshtoken 
+        const refreshToken = crypto.randomBytes(64).toString('hex');
+
+
+        //tạo session mới để lưu refreshtoken 
+        await Session.create({
+            userId:user._id,
+            refreshToken, 
+            expiresAt:new Date(Date.now() + REFRESH_TOKEN_TTL)
+        })
+
+        //trả refresh token trong cookie
+        res.cookie('refreshToken',refreshToken,{
+            httpOnly:true,//ko bị truy cập bới js
+            secure:true,//chỉ gửi qua https
+            sameSite:'none',//backend ,frondend deploy riêng
+            maxAge:REFRESH_TOKEN_TTL
+
+        })
+
+        //trả access token về trong res
+        return res.status(200).json({message:`Usser ${user.displayName} đã logged in `}, accessToken)
+
+    } catch (error) {
+        console.error('Lỗi khi gọi signIn', error);
+        return res.sendStatus(500).json({
+            message: "Lỗi hệ thống"
+        })
     }
 }
--- backend/src/routers/authRouter.js
+++ backend/src/routers/authRouter.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { signUp } from '../controllers/authController.js';
+import { signIn, signUp } from '../controllers/authController.js';
 
 const router = express.Router();
 
