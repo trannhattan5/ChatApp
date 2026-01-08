e9f7a3eb2507610597136dc50ce7e53fba664d0e
trannhattan5 <nhattan11205@gmail.com>
2026-01-08 21:37:00 +0700

    Cài đặt frontend với Vite + React Setup shadcn/ui, Tailwind CSS và theme

---
+++ frontend/src/App.tsx
@@ -0,0 +1,30 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+import SignInPage from './pages/SignInPage';
+import SignUpPage from './pages/SignUpPage';
+import ChatAppPage from './pages/ChatAppPage';
+import { Toaster } from 'sonner'
+
+function App() {
+
+  return (
+    <>
+      <Toaster richColors />
+      <BrowserRouter>
+        <Routes>
+          {/* public route */}
+          <Route path="signin" element={<SignInPage />} />
+          <Route path="signin" element={<SignUpPage />} />
+
+
+          {/* protected route */}
+          {/* todo: tạo protected route */}
+          <Route path="/" element={<ChatAppPage />} />
+
+
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/ChatAppPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ChatAppPage = () => {
+  return (
+    <div>
+      <h1>Ha</h1>
+    </div>
+  )
+}
+
+export default ChatAppPage
