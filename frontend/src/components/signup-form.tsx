a6ac283e2f38f33e9a7db28c5ebc003a9289ae9b
trannhattan5 <nhattan11205@gmail.com>
2026-01-08 22:45:55 +0700

    Validate form với thư viện Zod

--- frontend/src/components/signup-form.tsx
+++ frontend/src/components/signup-form.tsx
@@ -1,22 +1,41 @@
 import { cn } from "@/lib/utils"
 import { Card, CardContent } from "@/components/ui/card"
-import {
-  FieldDescription,
 
-} from "@/components/ui/field"
 import { Label } from "@radix-ui/react-label"
 import { Input } from "./ui/input"
 import { Button } from "./ui/button"
+import {useForm} from 'react-hook-form'
+import {zodResolver} from '@hookform/resolvers/zod' // giúp kết nối reacthookform 
+// zod kiểm tra dữ liệu; reacthookform lo sự kiện trạng thái của form
+
+import { z } from 'zod'
+const signUpSchema = z.object({
+  firstname: z.string().min(1, 'Tên bắt buộc phải có'),
+  lastname: z.string().min(1, 'Họ bắt buộc phải có'),
+  username: z.string().min(3, 'Tên đăng nhập phải có ít nhất 3 ký tự'),
+  email: z.email("Email không hợp lệ"),
+  password: z.string().min(6, 'Mật khẩu ít nhất 6 ký tự')
+})
+type SignUpFormValues = z.infer<typeof signUpSchema>
 
 export function SignupForm({
   className,
   ...props
 }: React.ComponentProps<"div">) {
+
+  //theo dõi giá trị ô input - chạy khi người dùng bấm đăng ký -lấy (error nếu input ko hợp lệ-khi nào form trong quá trình gửi dữ liệu)
+  const {register,handleSubmit,formState:{errors,isSubmitting}}=useForm<SignUpFormValues>({
+    resolver:zodResolver(signUpSchema)
+  });
+  const onSubmit = async(data :SignUpFormValues)=>{
+    //gọi api backend để sign
+  }
+
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card className="overflow-hidden p-0 border-border">
         <CardContent className="grid p-0 md:grid-cols-2">
-          <form className="p-6 md:p-8">
+          <form className="p-6 md:p-8" onSubmit={handleSubmit(onSubmit)}>
             <div className="flex flex-col gap-6">
               {/* header -logo */}
               <div className="flex flex-col items-center text-center gap-2">
@@ -34,16 +53,23 @@ export function SignupForm({
                   <Label htmlFor="lastname" className="block text-sm">
                     Họ
                   </Label>
-                  <Input type="text" id="lastname" />
+                  {/* ...: spread : tự gán event cần thiết reacthookform hoạc động */}
+                  <Input type="text" id="lastname" {...register("lastname")}/> 
                   {/* todo:error message */}
+                  {errors.lastname &&(
+                    <p className="text-destructive text-sm">{errors.lastname.message}</p>
+                  )}
                 </div>
 
                 <div className="space-y-2">
                   <Label htmlFor="firstname" className="block text-sm">
                     Tên
                   </Label>
-                  <Input type="text" id="firstname" />
+                  <Input type="text" id="firstname"  {...register("firstname")} />
                   {/* todo:error message */}
+                    {errors.firstname &&(
+                    <p className="text-destructive text-sm">{errors.firstname.message}</p>
+                  )}
                 </div>
               </div>
               {/* username */}
@@ -51,27 +77,36 @@ export function SignupForm({
                 <Label htmlFor="username" className="block text-sm">
                   Tên đăng nhập
                 </Label>
-                <Input type="text" id="username" placeholder="Chatapp" />
+                <Input type="text" id="username" placeholder="Chatapp"  {...register("username")}/>
                 {/* todo:error message */}
+                  {errors.username &&(
+                    <p className="text-destructive text-sm">{errors.username.message}</p>
+                  )}
               </div>
               {/* email */}
               <div className="flex flex-col gap-3">
                 <Label htmlFor="email" className="block text-sm">
                   Email
                 </Label>
-                <Input type="email" id="email" placeholder="Chatapp@gmail.com" />
+                <Input type="email" id="email" placeholder="Chatapp@gmail.com"  {...register("email")}/>
                 {/* todo:error message */}
+                  {errors.email &&(
+                    <p className="text-destructive text-sm">{errors.email.message}</p>
+                  )}
               </div>
               {/* password */}
               <div className="flex flex-col gap-3">
                 <Label htmlFor="password" className="block text-sm">
                   Mật khẩu
                 </Label>
-                <Input type="password" id="password"/>
+                <Input type="password" id="password"  {...register("password")}/>
                 {/* todo:error message */}
+                  {errors.password &&(
+                    <p className="text-destructive text-sm">{errors.password.message}</p>
+                  )}
               </div>
               {/* nút đăng ký */}
-              <Button type="submit" className="w-full">Tạo tài khoản</Button>
+              <Button type="submit" className="w-full" disabled={isSubmitting}>Tạo tài khoản</Button>
               <div className="text-center text-sm">Đã có tài khoản ?{""}
                 <a href="/signin" className="underline underline-offset-4">Đăng nhập</a>
               </div>
--- frontend/src/pages/SignUpPage.tsx
+++ frontend/src/pages/SignUpPage.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 
 const SignUpPage = () => {
   return (
-     <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+     <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10 absolute inset-0 z-0 "style={{
+background: "radial-gradient(125% 125% at 50% 10%, #fff 40%, #7c3aed 100%)",
+}}>
       <div className="w-full max-w-sm md:max-w-4xl">
         <SignupForm />
       </div>
